cc6d97710f019841f0eeb3c6022103113aafa4be
Martdev1963 <martdev1963@gmail.com>
2026-01-08 12:29:35 -0500

    Implement Card component with property details and link functionality

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,7 +1,19 @@
-const Card = () => {
+import Link from 'next/link'
+
+
+const Card = ({
+    propertyName,
+    slug,
+    rentalPrice,
+    beds,
+    image
+}) => {
+
     return (
-        <></>
+        <Link href={`/property/${slug}`}>
+            <div>{propertyName}</div> 
+        </Link>
     )
 } 
 export default Card 
-// vid_time: 22:02 / 01:24:05
+// vid_time: 46:22 / 01:24:05
